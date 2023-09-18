442d424203d9905bea84be97aaf92bd6185f480d
ogawa-kazuya <139742057+ogawa-kazuya@users.noreply.github.com>
2023-09-18 17:17:39 +0900

    2-4

---
+++ JavaStudy/2-4/src/study/Study.java
@@ -0,0 +1,23 @@
+package study;
+
+
+//②Fruits.javaをインポートしなさい。
+import fruits.Fruits;
+/**
+* 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
+*
+* 問①〜問③まであります。
+* 全て回答し、課題にある画像と同じ表示になるようにしてください。
+*
+*/
+public class Study {
+
+  public static void main(String[] args) {
+
+      new Fruits();
+	  Fruits.printFruits("レモン", 20);
+	 
+
+  }
+}
+
